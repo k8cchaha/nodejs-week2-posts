9daf439841f69294d02d8e1fec29c02426ead7ab
Alex_Wang <k8cchaha@gmail.com>
2022-05-04 00:41:22 +0800

    add delete one

--- server.js
+++ server.js
@@ -79,6 +79,17 @@ const requestListener = async (req, res) => {
       })
     );
     res.end();
+  } else if (url.startsWith("/posts/") && method === "DELETE") {
+    const id = url.split("/").pop();
+    const posts = await Post.deleteOne({ _id: id });
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        status: "success",
+        posts,
+      })
+    );
+    res.end();
   } else if (method === "OPTION") {
     res.writeHead(200, headers);
     res.end();
